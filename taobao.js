57a3e6af927a269d763f7f08942eb5f6c5c37d4b
Alexandr <alexandrhr92@gmail.com>
2019-03-11 20:05:05 +0600

    Нормально

--- taobao.js
+++ taobao.js
@@ -1,7 +1,7 @@
 alert('qwe');
 
 // вкладка - [Детали продукта]
-function get_product_detail() {
+function get_product_detail_2() {
     var arr_product_detail = [];
     $("#attributes #J_AttrList #J_AttrUL").find('li').each(function(indx, element){
         var name = $(element).text();
