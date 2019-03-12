b1b7d0b9d8a283f9f7232b5278abeb250d565db4
Alexandr <alexandrhr92@gmail.com>
2019-03-12 14:52:58 +0600

    Очередной шаг

--- taobao.js
+++ taobao.js
@@ -7,11 +7,11 @@
     var html_block_result_preview = '' +
         '<div id="block_result_preview">' +
             '<div class="brp_title"><input type="text"/></div>'+
-            '<div class="brp_images"><div/>'+
-            '<div class="brp_price"><div/>'+
-            '<div class="brp_var_attr"><div/>'+
-            '<div class="brp_desc_attr"><div/>'+
-            '<div class="brp_desc_content"><div/>'+
+            '<div class="brp_images"></div>'+
+            '<div class="brp_price"></div>'+
+            '<div class="brp_var_attr"></div>'+
+            '<div class="brp_desc_attr"></div>'+
+            '<div class="brp_desc_content"></div>'+
         ''+
     '';
 
